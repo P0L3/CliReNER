01ddd5c060956c9875bc3d688a4572a65c385cda
P0L3 <andrija.2@hotmail.com>
2025-11-25 12:17:14 +0100

    Add hf_dataset loading for GLiNER finetune

--- EXPERIMENTS/finetune.py
+++ EXPERIMENTS/finetune.py
@@ -9,4 +9,19 @@ parser.add_argument("--model_type", type=str)
 
 args = parser.parse_args()
 
-###
+if args.model_type == "GLINER":
+    # import json
+    # import random
+    # from datasets import load_dataset
+    # from seqeval.metrics.sequence_labeling import get_entities
+    # import re
+    # from collections import Counter
+    # # import matplotlib.pyplot as plt
+    # import pandas as pd
+    # from typing import List, Dict
+
+    # from dataset_processing import *
+    print("GLINER")
+    
+elif args.model_type == "SPANMARKER":
+    print("SPANMARKER")
