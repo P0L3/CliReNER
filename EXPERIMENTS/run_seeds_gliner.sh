a246e32db77c3805ec987346a8ab820049b85e2a
P0L3 <andrija.2@hotmail.com>
2026-09-03 08:27:43 +0200

    PC1 Starts training for GLiNER_CliReBERT_6es1

--- EXPERIMENTS/run_seeds_gliner.sh
+++ EXPERIMENTS/run_seeds_gliner.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MODEL="EXPERIMENTS/models/GLINER_CUSTOM/clirebert_clirevocab_uncased/stage2_s301202/checkpoint-final"
+MODEL="EXPERIMENTS/models/GLINER_CUSTOM/clirebert_clirevocab_uncased_6estage1/stage2_s301202/GLiNER_CliReBERT_6es1"
 DATA="P0L3/CliReNER_v_1_1_28_SILVER"
 CONFIG="EXPERIMENTS/gliner_config.json"
 PROJECT="CLIRENER_SILVER_SEEDS"
-BASE_NAME="GLiNER_Medium"
+BASE_NAME="GLiNER_CliReBERT_6es1"
 
-# Loop through seeds
+# Loop through seeds 0 42
 for SEED in 0 42 3012 33 131
 do
     echo "-----------------------------------"
